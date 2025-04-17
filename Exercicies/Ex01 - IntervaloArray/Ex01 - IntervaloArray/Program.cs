9e3e808694e45fdcb7624ff8eb38c277a902172c
Gustavo Laurindo dos Santos <104037685+lawrindovsk@users.noreply.github.com>
2025-04-16 21:45:03 -0300

    V 1.1 - Mais commits
    Aqui tem mais projetos onde estive treinando e estudando Csharp/.NET, aonde aprendo um pouco mais de:
    
    - Array
    - Calculos Bancarios (ficticio)
    - Math

---
+++ Exercicies/Ex01 - IntervaloArray/Ex01 - IntervaloArray/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01___IntervaloArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("by Gustavo Laurindo");
+
+            Console.WriteLine("Informe o tamanho do array:");
+            int tArray = Convert.ToInt32(Console.ReadLine());
+            int[] ArrayNumeros = new int[tArray];
+
+            for (int i = 0; i < tArray; i++)
+            {
+                Console.WriteLine($"informe o valor [0-100] do número na posição {i}: ");
+                ArrayNumeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int frenq1 = 0;
+            int frenq2 = 0;
+            int frenq3 = 0;
+            int frenq4 = 0;
+
+
+            foreach (int num in ArrayNumeros)
+            {
+                if (num >= 0 && num <= 25)
+                {
+                    frenq1++;
+                }
+
+                if (num >= 26 && num <= 50)
+                {
+                    frenq2++;
+                }
+
+                if (num >= 51 && num <= 75)
+                {
+                    frenq3++;
+                }
+
+                if (num >= 76 && num <= 100)
+                {
+                    frenq4++;
+                }
+
+                Console.WriteLine($"Quantidade de números  entre [0-25] é: {frenq1}");
+                Console.WriteLine($"Quantidade de números  entre [26-50] é: {frenq2}");
+                Console.WriteLine($"Quantidade de números  entre [51-75] é: {frenq3}");
+                Console.WriteLine($"Quantidade de números  entre [76-100] é: {frenq4}");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Exercicies/Ex04 - TrintaNomes/Ex04 - TrintaNomes/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04___TrintaNomes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int opcao;
+            //Criar nosso array de nomes
+
+            string[] nomes = new string[5];
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.Write($"Digite o {i + 1}º nome: ");
+                nomes[i] = Console.ReadLine().ToUpper();
+            }
+
+            Console.WriteLine("++++++++++++ Menu ~ by Gustavo Laurindo ++++++++++++");
+            Console.WriteLine("1. Exibir todos os Nomes;");
+            Console.WriteLine("2. Exibir o Maior dos Nomes;");
+            Console.WriteLine("3. Para exibir o primeiro nome de todos lidos;");
+            Console.WriteLine("4. Exibir os nomes que se iniciam com vogais;");
+            Console.WriteLine("0. Sair");
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Digite sua opção: ");
+                opcao = int.Parse(Console.ReadLine().ToUpper());
+                while (opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 0)
+                {
+                    Console.Write("Opção Incorreta, tente novamente com a opção correta: ");
+                    opcao = int.Parse(Console.ReadLine().ToUpper());
+                    Console.WriteLine();
+                }
+                if (opcao == 1)
+                {
+                    Console.WriteLine("Exibindo todos os nomes escritos: ");
+                    for (int i = 0; i < nomes.Length; i++)
+                    {
+                        if (i == 0)
+                        {
+                            Console.Write("[");
+                        }
+                        Console.Write(nomes[i]);
+                        if (i + 1 != nomes.Length)
+                        {
+                            Console.Write(", ");
+                        }
+                        if (i == nomes.Length - 1)
+                        {
+                            Console.Write("]\n");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                if (opcao == 2)
+                {
+                    Console.WriteLine($"O(s) maior(es) nome(s) digitado(s) foi: ");
+                    int maior = 0;
+                    for (int i = 0; i < nomes.Length; i++)
+                    {
+                        if (nomes[i].Length > nomes[maior].Length)
+                        {
+                            maior = i;
+                        }
+                    }
+                    Console.WriteLine(nomes[maior]);
+                    for (int i = 0; i < nomes.Length; i++)
+                    {
+                        if (nomes[i].Length == nomes[maior].Length && nomes[i] != nomes[maior])
+                        {
+                            Console.WriteLine(nomes[i]);
+                        }
+                    }
+                    Console.WriteLine($"O maior nome digitado foi {nomes[maior]}");
+                    Console.WriteLine();
+                }
+                if (opcao == 3)
+                {
+                    Console.WriteLine("Exibindo os primeiros nomes escritos de cada nome: ");
+                    for (int i = 0; i < nomes.Length; i++)
+                    {
+                        string[] pNome = nomes[i].Split();
+                        Console.WriteLine(pNome[0]);
+                    }
+                    Console.WriteLine();
+                }
+                if (opcao == 4)
+                {
+                    Console.WriteLine("Exibindo nomes que começam com vogais: ");
+                    for (int i = 0; i < nomes.Length; i++)
+                    {
+                        if (nomes[i][0] == 'A' || nomes[i][0] == 'E' || nomes[i][0] == 'I' || nomes[i][0] == 'O' || nomes[i][0] == 'U')
+                        {
+                            Console.WriteLine(nomes[i]);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                if (opcao == 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    break;
+                }
+
+            }
+        }
+    }
+}
