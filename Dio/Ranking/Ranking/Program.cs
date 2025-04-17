785d6126c6f106504a42e6ef487664cdf52008bf
Gustavo Laurindo dos Santos <104037685+lawrindovsk@users.noreply.github.com>
2025-04-16 21:54:47 -0300

    V 1.2 - Commit de Desafios DIO

---
+++ Dio/Ranking/Ranking/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranking
+{
+    class Program
+    {
+        //Níveis de Herói em CSHARP;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do herói:? ");
+            string nameHero = Console.ReadLine();
+
+            Console.WriteLine("Digite a quantidade de experiência do héroi: ");
+            int xp = int.Parse(Console.ReadLine());
+
+            string level;
+
+            if (xp < 1000)
+                level = "Ferro";
+            else if (xp <= 2000)
+                level = "Bronze";
+            else if (xp <= 5000)
+                level = "Prata";
+            else if (xp <= 7000)
+                level = "Ouro";
+            else if (xp <= 8000)
+                level = "Platina";
+            else if (xp <= 9000)
+                level = "Ascendente";
+            else if (xp <= 10000)
+                level = "Imortal";
+            else
+                level = "Radiante";
+
+            Console.WriteLine($"O Herói de nome {nameHero} está no Nível {level}");
+            Console.ReadKey();
+                    
+
+        }
+    }
+}
---
+++ Dio/Ranking_Two/Ranking_Two/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ranking_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int qtdWin;
+            int qtDft;
+            int totalStatus;
+
+             //ferro = 10;
+             //bronze = 20;
+             //prata = 50;
+             //ouro = 80;
+             //diamante = 90;
+             //lendario = 100;
+             //Imortal = 101;
+
+            Console.WriteLine("Digite seu nome:");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"{name}, digite sua quantidade de Vitórias que você tem atualmente:");
+            qtdWin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{name}, digite sua quantidade de Derrotas que você tem atualmente:");
+            qtDft = int.Parse(Console.ReadLine());
+
+            totalStatus = (qtdWin - qtDft);
+            string result;
+
+
+            if (totalStatus < 10)
+                result = "Ferro";
+            else if (totalStatus <= 20)
+                result = "Bronze";
+            else if (totalStatus <= 50)
+                result = " Prata";
+            else if (totalStatus <= 80)
+                result = "Ouro";
+            else if (totalStatus <= 90)
+                result = "Diamante";
+            else if (totalStatus <= 100)
+                result = "Lendário";
+            else
+                result = "IMORTAL";
+
+            Console.WriteLine ($"{name},seu Ranking atualmente é {result}, e tem um saldo de vitórias de {totalStatus}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
